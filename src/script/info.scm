efb264275c03b03bc62ec766863585bad9e29e24
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-06-02 10:16:38 +0200

    Adding the license header in info.scm.

--- src/script/info.scm
+++ src/script/info.scm
@@ -1,3 +1,19 @@
+;
+; This file is part of Pack My Sprites.
+;
+; Pack My Sprites is free software: you can redistribute it and/or modify it
+; under the terms of the GNU General Public License as published by
+; the Free Software Foundation, version 3 of the License.
+;
+; Pack My Sprites is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with Pack My Sprites.  If not, see <http://www.gnu.org/licenses/>.
+;
+
 ; \brief Displays the items of a list on a single line, separated by spaces..
 ; \param l The list to display
 ;
